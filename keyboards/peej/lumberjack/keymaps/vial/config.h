ae807176334b38bbc68df36f9dea7cb9f34d43ad
qwahtroh <armand.grant.2000@gmail.com>
2022-05-14 22:23:58 -0500

    Updated Lumberjack settings

--- keyboards/peej/lumberjack/keymaps/vial/config.h
+++ keyboards/peej/lumberjack/keymaps/vial/config.h
@@ -4,7 +4,4 @@
 
 #define VIAL_KEYBOARD_UID {0xBE, 0x78, 0x4B, 0x49, 0x48, 0xB7, 0xA9, 0x77}
 
-
-/* this is the left upper and left lower key */
-#define VIAL_UNLOCK_COMBO_ROWS { 0, 4 }
-#define VIAL_UNLOCK_COMBO_COLS { 0, 0 }
+#define DYNAMIC_KEYMAP_LAYER_COUNT 2
